a6780db6643f4a8f4900ebcd984b2c20e2e66019
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-11-28 19:34:06 +0700

    project-initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p className="introduce">Welcome to your new single-spa application</p>
+    </div>
+  );
+}
+
+export default App;
