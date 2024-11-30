bde11d78977c3d8ebd9f527c8aa009e1c1701869
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-11-30 11:39:11 +0700

    props

--- src/components/Hobby.jsx
+++ src/components/Hobby.jsx
@@ -1,12 +1,14 @@
+import ItemHobby from "./ItemHobby";
+
 export function Hobby() {
   return (
     <div>
       <h2>Danh sách sở thích</h2>
       <ul>
-        <li>Lập trình</li>
-        <li>Cờ vua</li>
-        <li>Đọc sách</li>
-        <li>Xem phim</li>
+        <ItemHobby stt="1" icon="💻" title="Lập trình" />
+        <ItemHobby stt="2" icon="♟️" title="Cờ vua" />
+        <ItemHobby stt="3" icon="📖" title="Đọc sách" />
+        <ItemHobby stt="4" icon="📺" title="Xem phim" />
       </ul>
     </div>
   );
