dc01e2552de9f876eb533e92bea001918adfffe7
bin2812 <lquanghuy218@gmail.com>
2024-12-07 10:50:28 +0700

    text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import "./index.css";
-import { Blog } from "./components/Blog";
-import { Hobby } from "./components/Hobby";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { Person } from "./components/Person";
-import { Profile } from "./components/Profile";
+// import { Blog } from "./components/Blog";
+// import { Hobby } from "./components/Hobby";
+// import { Footer } from "./components/Footer";
+// import { Header } from "./components/Header";
+// import { Person } from "./components/Person";
+// import { Profile } from "./components/Profile";
 import { TodoList } from "./components/TodoList";
 import { Calculator } from "./components/Calculator";
 // react thực hiện render theo dạng tree, gốc, nhánh
@@ -13,16 +13,16 @@ import { Calculator } from "./components/Calculator";
 function App() {
   return (
     <>
-      {/* <Header /> */}
-      {/* <h1>Website Reactjs</h1> */}
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* <Header /> 
+       <h1>Website Reactjs</h1> 
+       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Profile />
       <Person />
       <Hobby />
       <Blog />
-      <Footer /> */}
+      <Footer />
       <TodoList />
-      <Calculator />
+      <Calculator /> */}
     </>
   );
 }
