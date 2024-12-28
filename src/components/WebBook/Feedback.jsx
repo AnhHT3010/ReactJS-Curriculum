e69070de697e354cf0f877f31dd941939d2bc404
AnhHT3010 <trunganhhoang2001@gmail.com>
2024-12-28 12:06:52 +0700

    feedback

--- src/components/WebBook/Feedback.jsx
+++ src/components/WebBook/Feedback.jsx
@@ -1,13 +1,16 @@
 const Feedback = () => {
   return (
-    <div className="bg-[url('https://lh3.googleusercontent.com/JrYoWdtj0XTTthdmJz5ADcpm3j_srI6_TuA0zSDGytH44nUECfFsGljBzpwdyQRixA9X7Pgc2COtB2sjlI7fM3qzcr_LijoN')] text-center w-full px-[10%] py-[5%] bg-no-repeat bg-cover">
-      <div classname="w-[400px] bg-white">
-        <p className="text-gray-500  "> What people saying!</p>
+    <div className="mb- bg-[url('https://lh3.googleusercontent.com/JrYoWdtj0XTTthdmJz5ADcpm3j_srI6_TuA0zSDGytH44nUECfFsGljBzpwdyQRixA9X7Pgc2COtB2sjlI7fM3qzcr_LijoN')] text-center w-full px-[10%] py-[5%] bg-no-repeat bg-cover">
+      <div className="w-[45%] p-20 bg-white rounded-2xl">
+        <p className="text-gray-500 border-b-2 border-red-600">
+          {" "}
+          What people saying!
+        </p>
         <p className="">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
           tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Lorem
           ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
-          luctus nec ullamcorper mattis, pulvinar dapibus leo."
+          luctus nec ullamcorper mattis, pulvinar dapibus leo.
         </p>
       </div>
     </div>
