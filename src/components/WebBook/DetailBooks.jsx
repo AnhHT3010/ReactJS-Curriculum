873d66e1f2bd8ff80905d949fa4b11f86b900226
bin2812 <lquanghuy218@gmail.com>
2025-01-09 20:39:05 +0700

    detail

--- src/components/WebBook/DetailBooks.jsx
+++ src/components/WebBook/DetailBooks.jsx
@@ -3,26 +3,26 @@ import TabDetail from "./TabDetail";
 const DetailBooks = () => {
   return (
     <div className="px-[10%] py-24">
-      <div className="flex">
-        <div>
+      <div className="flex gap-8">
+        <div className="border-[1px] border-[#00000024] border-solid p-5 rounded-xl">
           <img
             src="https://lh3.googleusercontent.com/imD708U0BRsFiqrJqyEJRTpGNnHZPInN5cqJxRZ5Jx6tC-X3u6uifaPNsYzOEBmrF0Gx8g0PWhMT6NpsVxrzI8eJOb9UOtwNEQ=rw-w600"
             alt=""
           />
         </div>
-        <div>
-          <h2>Dire Wolf Stakes</h2>
+        <div className="border-[1px] border-[#00000024] border-solid p-5 rounded-xl flex-col flex gap-5 h-auto">
+          <h2 className="font-bold text-4xl">Dire Wolf Stakes</h2>
           <p>
-            <span>:Author:</span> Hailey Nguyen
+            <span className="text-gray-400 ">:Author:</span> Hailey Nguyen
           </p>
-          <hr />
-          <p>$513.00</p>
-          <p>
+          <hr className="w-2/3 " />
+          <p className="text-red-500 text-2xl py-3 font-medium">$513.00</p>
+          <p className="text-slate-600 text-sm">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
             tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
           </p>
-          <hr />
-          <button>
+          <hr className="w-2/3 py-2" />
+          <button className="bg-red-600 text-white w-fit px-7 py-2 rounded-full">
             <i className="fa-solid fa-basket-shopping"></i>Buy Now
           </button>
         </div>
--- src/components/WebBook/ShopByCategory.tsx
+++ src/components/WebBook/ShopByCategory.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { categoryData } from "../../constants/categories.const";
+import { useNavigate } from "react-router-dom";
 
 const ShopByCategory = () => {
+  const navigate = useNavigate();
   return (
     <div className="py-24 px-[10%]">
       <div className="flex flex-row justify-start items-center gap-x-24">
@@ -21,7 +23,12 @@ const ShopByCategory = () => {
             <div className="w-1/2 flex flex-col justify-evenly">
               <h3 className="font-semibold">{category.title}</h3>
               <p className="text-gray-500 text-xs">{category.description}</p>
-              <button className="text-sm w-fit group p-2">
+              <button
+                onClick={() => {
+                  navigate(`/${category.id}`);
+                }}
+                className="text-sm w-fit group p-2"
+              >
                 View more{" "}
                 <i className="fa-solid fa-angle-right text-orange-600 group-hover:ml-3 transform transition-all duration-200 ease-in-out"></i>
               </button>
