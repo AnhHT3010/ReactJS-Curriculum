99a7f76a3cdab5258a0fa1ecc400828058f356e2
bin2812 <lquanghuy218@gmail.com>
2025-01-16 18:37:37 +0700

    review

--- src/components/WebBook/TabReviews.jsx
+++ src/components/WebBook/TabReviews.jsx
@@ -4,21 +4,35 @@ const TabReviews = ({ reviews }) => {
   return (
     <div>
       <h2>Reviews</h2>
-      {/* <ul>
-        {reviews.map((userReview, index) => (
-          <>
-            <li key={index}>{userReview.name}</li>
-            <li key={index}>{userReview.review}</li>
-          </>
+      <ul>
+        {reviews.map((review) => (
+          <li
+            key={review.id}
+            style={{ marginBottom: "20px", listStyleType: "none" }}
+          >
+            <strong>{review.name}</strong>
+
+            <p>{review.review}</p>
+
+            <div>
+              {"⭐".repeat(review.rating)} {"❌".repeat(5 - review.rating)}
+            </div>
+          </li>
         ))}
-      </ul> */}
-      <div></div>
+      </ul>
     </div>
   );
 };
 
 TabReviews.propTypes = {
-  reviews: PropTypes.arrayOf(PropTypes.string).isRequired,
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      review: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 
 export default TabReviews;
